bf235be4ab301a6bf543ff4fe40145559bfc5a5f
treysiz <99376209+treysiz@users.noreply.github.com>
2025-11-20 07:25:18 -0500

    Update check.php

--- public/check.php
+++ public/check.php
@@ -1,4 +1,15 @@
 <?php
-echo "JSON_FILE = " . JSON_FILE . "<br><br>";
-echo nl2br(file_get_contents(JSON_FILE));
-?>
+require "index.php";  // 引入配置
+
+echo "<h2>JSON 测试文件</h2>";
+echo "JSON_FILE = <b>" . JSON_FILE . "</b><br><br>";
+
+if (file_exists(JSON_FILE)) {
+    echo "✔ 文件存在<br>";
+    echo "<hr>";
+    echo "<pre>";
+    echo htmlspecialchars(file_get_contents(JSON_FILE));
+    echo "</pre>";
+} else {
+    echo "❌ 文件不存在：". JSON_FILE;
+}
